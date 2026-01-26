0977170fea19ead2035c991fd22c68faacc1769c
jidanyu <jidanyu1101@gmail.com>
2026-01-26 14:05:59 +0800

    feat: change theme to onedark pro

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,8 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.guifont = "JetBrainsMono Nerd Font:h10" -- 设置 Nerd Font 字体和字号
-
+-- vim.opt.guifont = "JetBrainsMono Nerd Font:h10" -- 设置 Nerd Font 字体和字号
+vim.o.guifont = "Consolas Nerd Font:h12"
 -- 只在 Neovide GUI 下生效
 if vim.g.neovide then
   -- 启动时全屏
