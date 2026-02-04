a18c370ee3e3292e1b63b81d78d823a47bc26dad
jidanyu <jidanyu1101@gmail.com>
2026-02-04 13:23:04 +0800

    fix: update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,7 +3,11 @@
 -- Add any additional options here
 
 -- vim.opt.guifont = "JetBrainsMono Nerd Font:h10" -- 设置 Nerd Font 字体和字号
-vim.o.guifont = "Consolas Nerd Font:h12"
+if vim.fn.has("win32") == 1 then
+  vim.o.guifont = "Consolas Nerd Font:h12"
+else
+  vim.o.guifont = "JetBrainsMono Nerd Font:h12"
+end
 -- 只在 Neovide GUI 下生效
 if vim.g.neovide then
   -- 启动时全屏
@@ -13,6 +17,11 @@ end
 -- 固定启动目录（无论从哪里打开 nvim）
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
-    vim.cmd.cd(vim.fn.expand("D:/")) -- 改成你想要的目录
+    -- 仅在 Windows 上切换到 D:，避免 Linux 没有 D: 盘导致报错
+    if vim.fn.has("win32") == 1 and vim.fn.isdirectory("D:/") == 1 then
+      vim.cmd.cd(vim.fn.expand("D:/")) -- 改成你想要的目录
+    end
   end,
 })
+
+--
